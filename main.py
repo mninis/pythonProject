2dc1e39dd3b842fb75ef5142cfc29ea6a7be687b
iNinis <ininis93@hotmail.com>
2023-02-13 21:23:25 +0200

    my first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/List_of_highest-paid_film_actors"
+url_txt = requests.get(url).text
+s = BeautifulSoup(url_txt, "html.parser")
+print(s.title)
+print(s.title.string)
+
+
+
+
+
+
